ccb6731ec3d8b672aa543fe5fa9334f8a3cb495c
Eugene Rayner <rayner.euge@gmail.com>
2023-04-05 09:25:12 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   images: {
-    loader: 'akamai',
-    path: '',
-  },
+        unoptimized: true
+  }
 })
 
 module.exports = withNextra()
