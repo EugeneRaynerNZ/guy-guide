fba3bebba519803899f1ace56a4224b005386c07
unknown <rayner.euge@gmail.com>
2023-04-05 22:31:21 +1200

    added extra pages to diet and attempted to add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '../styles.css'
+import Script from 'next/script';
+// import { GoogleAnalytics } from "nextjs-google-analytics";
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      {/* <GoogleAnalytics trackPageViews /> */}
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+
+      <Script strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
