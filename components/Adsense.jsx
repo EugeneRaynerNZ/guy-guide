40deafd44551284f5c09cfa724dc6bd15e8f9213
unknown <rayner.euge@gmail.com>
2023-04-07 16:57:50 +1200

    attempted to add adsense to fix bug

--- components/Adsense.jsx
+++ components/Adsense.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Script from 'next/script';
 
 export default function Adsense() {
   const loadAds = () => {
@@ -17,11 +18,20 @@ export default function Adsense() {
 
   return (
     // In article Ad Template
-    <ins className="adsbygoogle"
+    <>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1684783406695529"
+     crossOrigin="anonymous"></Script>
+
+      <ins className="adsbygoogle"
       style={{display: 'block'}}
       data-ad-client="ca-pub-1684783406695529"
       data-ad-slot="4686319427"
       data-ad-format="auto"
       data-full-width-responsive="true"></ins>
+
+      <Script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </Script>
+    </>
   );
 }
