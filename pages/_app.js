fd0524dd4fc9ce0971592e69ad502dd007f0731a
unknown <rayner.euge@gmail.com>
2023-04-10 20:08:13 +1200

    fixed crossOrigin

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ const App = ({ Component, pageProps }) => {
       {/* Adsense */}
       <script 
         async
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1684783406695529"> 
       </script>
 
